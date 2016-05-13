dd5d2d8c33aa04d8947bda71b3685aeca4bf5dfc
Amit Shah <amit.shah@infostretch.com>
2016-05-13 17:49:18 +0530

    Modified test case

--- tests/sampleTest.php
+++ tests/sampleTest.php
@@ -23,11 +23,6 @@ class sampleTest extends PHPUnit_Extensions_Selenium2TestCase
 		$this->assertEquals("PHP - Jenkins demo", $this->title());
 	}
 	
-	/**
-     * Sample Test case
-     *
-	 * @test
-	 */	
 	public function tearDown()
 	{
 		$this->url("http://".getenv('SELENIUM_SERVER_IP').":".getenv('SELENIUM_SERVER_PORT')."/selenium-server/driver/?cmd=shutDownSeleniumServer");
